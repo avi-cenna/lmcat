76b350dd480162bd3558c9a34a6f3af49052542b
N <47500890+avi-cenna@users.noreply.github.com>
2025-01-20 18:14:23 -0500

    refactor: Remove unused tiktoken-related code and commented-out functions

--- stats.go
+++ stats.go
@@ -3,9 +3,8 @@ package main
 import (
 	_ "github.com/h2non/filetype/matchers"
 	"github.com/jedib0t/go-pretty/v6/table"
-	"github.com/pkoukk/tiktoken-go"
 	"github.com/urfave/cli"
-	"log"
+	_ "log"
 	"os"
 	"path/filepath"
 	"slices"
@@ -13,139 +12,20 @@ import (
 	"strings"
 )
 
-var (
-	tke *tiktoken.Tiktoken
-)
-
-func init() {
-	encoding := "cl100k_base"
-	var err error
-	tke, err = tiktoken.GetEncoding(encoding)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
 func RunStats(cliCtx *cli.Context) error {
 	return nil
 }
 
-//func RunStats(cliCtx *cli.Context) error {
-//	// Get current working directory
-//	cwd, err := os.Getwd()
+//func CountTokensFilePath(filePath string) (int, error) {
+//	content, err := ReadFile(filePath)
 //	if err != nil {
-//		return fmt.Errorf("error getting current working directory: %w", err)
+//		return 0, err
 //	}
-//	fmt.Println("Current working directory:", cwd)
-//
-//	extCounts := make(map[string]int)
-//	extTokens := make(map[string]int)
-//
-//	files, err := FindFiles(cliCtx.String("glob"), cliCtx.String("regex"))
-//	if err != nil {
-//		return fmt.Errorf("error getting files: %w", err)
-//	}
-//
-//	for _, f := range files {
-//		tokenCount, err := CountTokensFilePath(f)
-//		if err != nil {
-//			log.Printf("Error counting extTokens in file %s: %v", f, err)
-//		}
-//
-//		ext := strings.ToLower(filepath.Ext(f))
-//		if ext != "" {
-//			extCounts[ext]++
-//			extTokens[ext] += tokenCount
-//		} else {
-//			extCounts[filepath.Base(f)]++
-//			extTokens[filepath.Base(f)] += tokenCount
-//		}
-//	}
-//
-//	printCountsAndTokens(extCounts, extTokens)
-//	return nil
+//	return CountTokensInText(content)
 //}
 
-//func RunStatsParallel(cliCtx *cli.Context) error {
-//	// Get current working directory
-//	cwd, err := os.Getwd()
-//	if err != nil {
-//		return fmt.Errorf("error getting current working directory: %w", err)
-//	}
-//	fmt.Println("Current working directory:", cwd)
-//
-//	extCounts := make(map[string]int)
-//	extTokens := make(map[string]int)
-//	var mu sync.Mutex // Mutex to protect concurrent map access
-//
-//	files, err := FindFiles(cliCtx.String("glob"), cliCtx.String("regex"))
-//	if err != nil {
-//		return fmt.Errorf("error getting files: %w", err)
-//	}
-//
-//	// Create a channel to receive results
-//	results := make(chan struct {
-//		ext   string
-//		count int
-//	}, len(files))
-//
-//	// Use a WaitGroup to wait for all goroutines to finish
-//	var wg sync.WaitGroup
-//
-//	// Process files concurrently
-//	for _, f := range files {
-//		wg.Add(1)
-//		go func(filePath string) {
-//			defer wg.Done()
-//
-//			tokenCount, err := CountTokensFilePath(filePath)
-//			if err != nil {
-//				log.Printf("Error counting tokens in file %s: %v", filePath, err)
-//				return
-//			}
-//
-//			ext := strings.ToLower(filepath.Ext(filePath))
-//			if ext == "" {
-//				ext = filepath.Base(filePath)
-//			}
-//
-//			results <- struct {
-//				ext   string
-//				count int
-//			}{ext, tokenCount}
-//		}(f)
-//	}
-//
-//	// Close the results channel when all goroutines are done
-//	go func() {
-//		wg.Wait()
-//		close(results)
-//	}()
-//
-//	// Collect results
-//	for result := range results {
-//		mu.Lock()
-//		extCounts[result.ext]++
-//		extTokens[result.ext] += result.count
-//		mu.Unlock()
-//	}
-//
-//	printCountsAndTokens(extCounts, extTokens)
-//	return nil
-//}
-
-func CountTokensFilePath(filePath string) (int, error) {
-	content, err := ReadFile(filePath)
-	if err != nil {
-		return 0, err
-	}
-	return CountTokensInText(content)
-}
-
-func CountTokensInText(text []byte) (int, error) {
-	tokens := tke.Encode(string(text), nil, nil)
-	return len(tokens), nil
-}
+//TODO: flesh this out, paying attention to the example in examples.md
+//func CountTokensInText(...)
 
 func printCountsAndTokens(extCounts map[string]int, extTokens map[string]int) {
 	sortedKeys := make([]string, 0, len(extCounts))
@@ -163,7 +43,6 @@ func printCountsAndTokens(extCounts map[string]int, extTokens map[string]int) {
 	t.Render()
 }
 
-// /
 func extensionOrBase(filePath string) string {
 	ext := strings.ToLower(filepath.Ext(filePath))
 	if ext != "" {
