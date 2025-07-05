751c05738af0c2ccb3012c58519f0641921315ec
N <47500890+avi-cenna@users.noreply.github.com>
2025-07-05 15:00:14 -0400

    claude

---
+++ count.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"sync"
+
+	"github.com/rs/zerolog/log"
+
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+
+	"github.com/jedib0t/go-pretty/v6/table"
+)
+
+type StatsFileResult struct {
+	Location string
+	//Content    []byte
+	TokenCount int
+}
+
+func RunStats(command *HiArgs, pipeData string) error {
+	countTokens := GetTokenFunc(command.approx)
+	var fileQueue chan string
+	if pipeData != "" {
+		fileQueue = ConvertPipeDataToChannel(pipeData, bufSize)
+	} else {
+		fileQueue = WalkFiles(bufSize)
+	}
+	resultQueue := make(chan *StatsFileResult, 100)
+	processFilesStats(command, fileQueue, resultQueue, countTokens)
+
+	done := make(chan struct{})
+	go func() {
+		extCounts := make(map[string]int)
+		extTokens := make(map[string]int)
+		for f := range resultQueue {
+			ext := extensionOrBase(f.Location)
+			extCounts[ext]++
+			extTokens[ext] += f.TokenCount
+		}
+		printCountsAndTokens(extCounts, extTokens)
+		close(done)
+	}()
+
+	<-done
+	return nil
+}
+
+func processFilesStats(
+	command *HiArgs,
+	fileQueue chan string,
+	resultQueue chan *StatsFileResult,
+	countTokens TokenFunc) {
+
+	wg := sync.WaitGroup{}
+	for filePath := range fileQueue {
+
+		wg.Add(1)
+		go func(filePath string) {
+			log.Debug().Str("file", filePath).Msg("Processing file")
+			defer wg.Done()
+			if !IsLikelyTextFile(filePath) {
+				return
+			}
+			if command.regexFilepath != nil && !command.regexFilepath.MatchString(filePath) {
+				return
+			}
+			fileBytes, err := os.ReadFile(filePath)
+			if err != nil {
+				log.Err(err).Str("file", filePath).Msg("Error reading file")
+				return
+			}
+			if command.regexContent != nil && !command.regexContent.Match(fileBytes) {
+				return
+			}
+			tokenCount := countTokens(fileBytes)
+			log.Debug().Str("file", filePath).Int("tokenCount", tokenCount).Msg("Counted tokens")
+			resultQueue <- &StatsFileResult{Location: filePath, TokenCount: tokenCount}
+		}(filePath)
+	}
+
+	wg.Wait()
+	close(resultQueue)
+}
+
+func processFilesSequential(
+	command *HiArgs,
+	fileQueue chan string,
+	resultQueue chan *StatsFileResult,
+	countTokens TokenFunc) {
+
+	for filePath := range fileQueue {
+		log.Debug().Str("file", filePath).Msg("Processing file")
+
+		if !IsLikelyTextFile(filePath) {
+			continue
+		}
+		if command.regexFilepath != nil && !command.regexFilepath.MatchString(filePath) {
+			continue
+		}
+
+		fileBytes, err := os.ReadFile(filePath)
+		if err != nil {
+			log.Err(err).Str("file", filePath).Msg("Error reading file")
+			continue
+		}
+
+		if command.regexContent != nil && !command.regexContent.Match(fileBytes) {
+			continue
+		}
+
+		tokenCount := countTokens(fileBytes)
+		log.Debug().Str("file", filePath).Int("tokenCount", tokenCount).Msg("Counted tokens")
+		resultQueue <- &StatsFileResult{Location: filePath, TokenCount: tokenCount}
+	}
+	close(resultQueue)
+
+}
+
+func printCountsAndTokens(extCounts map[string]int, extTokens map[string]int) {
+	sortedKeys := make([]string, 0, len(extCounts))
+	for ext := range extCounts {
+		sortedKeys = append(sortedKeys, ext)
+	}
+	slices.Sort(sortedKeys)
+
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Extension", "Count", "Tokens"})
+	for _, ext := range sortedKeys {
+		t.AppendRow(table.Row{ext, extCounts[ext], extTokens[ext]})
+	}
+	t.Render()
+}
+
+func extensionOrBase(filePath string) string {
+	ext := strings.ToLower(filepath.Ext(filePath))
+	if ext != "" {
+		return ext
+	}
+	return filepath.Base(filePath)
+}
